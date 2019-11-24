b6c581a33adb7789e00af5b1b705b82a4b89e378
mrbooboostank <57339742+mrbooboostank@users.noreply.github.com>
2019-11-24 15:27:52 -0600

    Add files via upload

---
+++ HUD caps name/lua/weapons/weapon_hudcapsname.lua
@@ -0,0 +1,44 @@
+SWEP.PrintName = "lowercase name"
+SWEP.Category = "HUD caps name"
+SWEP.DrawWeaponInfoBox = false
+SWEP.BounceWeaponIcon = false
+SWEP.SlotPos = 0
+
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+SWEP.UseHands = true
+SWEP.SwayScale = 0
+SWEP.BobScale = 0
+
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = 0
+SWEP.Primary.Automatic = true
+SWEP.Primary.Ammo = "none"
+
+SWEP.Secondary.ClipSize	= 0
+SWEP.Secondary.DefaultClip = 0
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo	= "none"
+
+SWEP.ViewModel	= "models/weapons/c_crowbar.mdl"
+SWEP.ViewModelFOV = 90
+SWEP.WorldModel	= "models/weapons/w_crowbar.mdl"
+
+function SWEP:Deploy()
+end
+
+function SWEP:Initialize()
+self.PrintName = "UPPERCASE NAME"
+end 
+
+function SWEP:Think()
+end 
+
+function SWEP:PrimaryAttack()
+end
+
+function SWEP:SecondaryAttack()
+end
+
+function SWEP:Reload()
+end
